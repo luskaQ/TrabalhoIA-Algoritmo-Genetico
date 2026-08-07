7c20d1c6647660dc5aab74048bbdb5c41b77d606
luskaQ <lucasfpolidorio@gmail.com>
2026-08-07 15:33:12 -0300

    adding skeleton

---
+++ genetico.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+//Hiperparametros
+#define NUM_DE_GERACOES 5000
+#define TAM_POPULACAO 100
+#define TAXA_CRUZAMENTO 0.1
+#define TAXA_MUTACAO 0.1
+
+using namespace std;
+
+void mutarPopulacao();
+
+void cruzarPopulacao();
+
+void calcularFitnessCromossomo();
+
+
+int main(){
+
+}
